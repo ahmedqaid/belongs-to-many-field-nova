571a6b14fda945b65d4f60677e6feab15e50d895
Ahmed Qaid <34524233+ahmedqaid@users.noreply.github.com>
2021-08-06 12:22:40 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
-use Benjacho\BelongsToManyField\Http\Controllers\ResourceController;
+use Sprii\BelongsToManyField\Http\Controllers\ResourceController;
 
 Route::get('/{resource}/options/{relationship}/{optionsLabel}/{dependsOnValue?}/{dependsOnKey?}', [ResourceController::class, 'index']);
+Route::get('/{resource}/combined-options/{relationship}/{optionsLabel}/{dependsOnValue?}/{dependsOnKey?}/{dependsOnGroupRelationship?}', [ResourceController::class, 'combined']);
